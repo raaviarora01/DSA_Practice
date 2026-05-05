f3933904fedc34422f258f996d48c766f2668743
raaviarora01 <aroraraavi01@gmail.com>
2026-05-05 23:47:05 +0530

    update: add findKthBit when kth bit is MSB approach 3 solution using bit manipulation

--- BitManipulation/CheckKthBit.java
+++ BitManipulation/CheckKthBit.java
@@ -13,7 +13,7 @@ public class CheckKthBit {
     }
 
     // Approach 2: Check kth bit from MSB - TC: O(1) and SC: O(1)
-    public char checkKthBitMSB(int n, int k) {
+    public char findKthBitMSB(int n, int k) {
         if (n == 0) return '0';
 
         // int len = (int)(Math.log(n)/Math.log(2)) + 1;  // Method 1: Calculate the number of bits using logarithm
@@ -23,5 +23,15 @@ public class CheckKthBit {
 
         int res = n & (1 << (len - k));
         return res != 0 ? '1' : '0';
+    }
+
+    // Approach 3: Check kth bit from MSB, but instead of calculating the number of bits, we can directly shift n to the right by (k-1) bits and check the least significant bit (LSB) - TC: O(1) and SC: O(1)
+    public char findKthBit2(int n, int k) {
+        if (n == 0) return '0';
+
+        int shift = 32 - k - Integer.numberOfLeadingZeros(n);
+        if (shift < 0) return '0'; // k > number of bits
+
+        return ((n >> shift) & 1) == 1 ? '1' : '0';
     }
 }
