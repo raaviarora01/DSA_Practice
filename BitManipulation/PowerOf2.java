ce5a227eb7b456f90942b62634d3a6b38bab8cfb
raaviarora01 <aroraraavi01@gmail.com>
2026-05-07 23:21:24 +0530

    feat: add Power of 2 solution using bit manipulation

---
+++ BitManipulation/PowerOf2.java
@@ -0,0 +1,12 @@
+/* Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x. */
+
+package BitManipulation;
+
+public class PowerOf2 {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        return (n & (n-1)) == 0;
+    }
+}
