afb033bacc0da4c5db6a43552533cfb5bff6f813
raaviarora01 <aroraraavi01@gmail.com>
2026-07-17 23:24:00 +0530

    fix: write correct method name and add complexity analysiis to recursive insertion sort

--- Recursion/RecursiveInsertionSort.java
+++ Recursion/RecursiveInsertionSort.java
@@ -1,5 +1,22 @@
+/* Recursive Insertion Sort Algorithm */
+
 package Recursion;
 
 public class RecursiveInsertionSort {
+
+    /* Time Complexity: O(n^2), Space Complexity: O(n) - recursion stack */
+    public static void recursiveInsertionSort(int[] nums, int i){
+        if(i == nums.length) return;
+
+        int j = i;
+        while(j>0 && nums[j-1] > nums[j]){
+            int temp = nums[j];
+            nums[j] = nums[j-1];
+            nums[j-1] = temp;
+            j--;
+        }
+
+        recursiveInsertionSort(nums, i+1);
+    }
 
 }
