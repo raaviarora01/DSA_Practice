0241673970c6841d1c6e494d1b7efeee38661719
raaviarora01 <aroraraavi01@gmail.com>
2026-08-17 22:43:48 +0530

    feat: add SingleNumber solution implementations using Optimal Approach

--- Arrays/SingleNumber.java
+++ Arrays/SingleNumber.java
@@ -44,7 +44,7 @@ public class SingleNumber {
     // Better Approach(Only Applicaple if the array contains only 0 or positive integers): Uses a Hash Array to count the occurrences of each number and finds the single number.
     // Time Complexity: O(n + max) - The method iterates over input array twice and over hash array once which is of length max
     // Space Complexity: O(max) - The method uses a hash array to store the occurrences of each number, which takes max length space in the best and worst case.
-    public int singleNumber(int[] nums) {
+    public int singleNumberHashArrayApproach(int[] nums) {
         int max = nums[0];
         for(int num : nums){
             if(num > max) max = num;
@@ -61,5 +61,18 @@ public class SingleNumber {
         }
 
         return -1;
+    }
+
+
+    // Optimal Approach: Uses xor to find the single element
+    // Time Complexity: O(n) - The method iterates over input array once to find the single number.
+    // Space Complexity: O(1) - The method uses a constant amount of space for variable to store the result
+    public int singleNumberOptimalApproach(int[] nums) {
+        int xor = 0;
+        for(int num : nums){
+            xor = xor ^ num;
+        }
+
+        return xor;
     }
 }
