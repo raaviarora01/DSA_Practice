14dc7fe6bd659c26ee8d91932fce6ab56dcf7d0d
raaviarora01 <aroraraavi01@gmail.com>
2026-08-19 20:46:02 +0530

    feat: add LongestSubarrayWithSumKPositives solution implementation using Better approach

--- Arrays/LongestSubarrayWithSumKPositives.java
+++ Arrays/LongestSubarrayWithSumKPositives.java
@@ -2,6 +2,9 @@
 
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubarrayWithSumKPositives {
 
     // Brute Force Approach: Iterates through the array and checks all possible sub-arrays to find the longest one that sums to k.
@@ -18,6 +21,34 @@ public class LongestSubarrayWithSumKPositives {
                 if(sum == k){
                     maxLength = Math.max(maxLength, j-i+1);
                 }
+            }
+        }
+
+        return maxLength;
+    }
+
+    // Better Approach: Use HashMap to store the sum of elements from the start of the array to the current index. If the difference between the current sum and k exists in the HashMap, it means there is a sub-array that sums to k.
+    // Time Complexity: O(n) - The method iterates over input array once.
+    // Space Complexity: O(n) - The method uses a HashMap to store the sum of elements from the start of the array to the current index, which can take up to n space in the worst case.
+    public int longestSubarrayWithSumKBetterApproach(int[] nums, int k) {
+        int maxLength = 0;
+        long sum = 0;
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(0L, -1);
+
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+
+            if(sum == k){
+                maxLength = i + 1;
+            }
+
+            if(map.containsKey(sum - k)){
+                maxLength = Math.max(maxLength, i - map.get(sum - k));
+            }
+
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
             }
         }
 
