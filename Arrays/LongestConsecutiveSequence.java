95c3bf35dab445b599835301c00efe331336d711
raaviarora01 <aroraraavi01@gmail.com>
2026-09-15 22:22:09 +0530

    feat: add solution implementation for LongestConsecutiveSequence in an array using better approach

--- Arrays/LongestConsecutiveSequence.java
+++ Arrays/LongestConsecutiveSequence.java
@@ -4,6 +4,8 @@ You must write an algorithm that runs in O(n) time. */
 
 package Arrays;
 
+import java.util.Arrays;
+
 public class LongestConsecutiveSequence {
 
     // Brute Force Approach: For each element in the array, check if it is the start of a sequence. If it is, then count the length of the sequence.
@@ -36,5 +38,32 @@ public class LongestConsecutiveSequence {
         }
 
         return false;
+    }
+
+    // Better Approach : Sort the array and then find the longest consecutive sequence.
+    // Time Complexity: O(nlogn) where n is the length of the given array, as we are sorting the array first and then traversing it once.
+    // Space Complexity: O(1) as we are not using any extra space.
+    public int longestConsecutiveBetter(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int lastSmaller = nums[0];
+        int cnt = 1;
+        int longest = 1;
+
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == lastSmaller + 1){
+                cnt++;
+                lastSmaller = nums[i];
+                longest = Math.max(cnt, longest);
+            } else if(nums[i] > lastSmaller + 1){
+                cnt = 1;
+                lastSmaller = nums[i];
+            }
+        }
+
+        return longest;
     }
 }
