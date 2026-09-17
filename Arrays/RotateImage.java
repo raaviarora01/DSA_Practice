b167c9970ecaecb6b5d62079f82d25833698aca5
raaviarora01 <aroraraavi01@gmail.com>
2026-09-17 14:10:56 +0530

    feat: add solution implementation for RotateImage using optimal approach

--- Arrays/RotateImage.java
+++ Arrays/RotateImage.java
@@ -23,4 +23,60 @@ public class RotateImage {
             matrix[i] = result[i].clone();
         }
     }
+
+    /* Transpose means swapping the rows and columns of a matrix. */
+
+    // Optimal Approach 1: Transpose the matrix and then reverse each row. Transpose the lower triangle(including diagonal) of the matrix by swapping the elements at (i, j) and (j, i). Then reverse each row of the matrix by swapping the elements at (i, j) and (i, n-j-1).
+    // Time Complexity: O(n^2) where n is the number of rows (or columns) in the given matrix. We are traversing the matrix twice, once to transpose it and once to reverse each row.
+    // Space Complexity: O(1) as we are not using any extra space.
+    public void rotateOptimalVersion1(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the lower triangle(including diagonal) by swapping
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row by swapping
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+
+    // Optimal Approach 2: Transpose the matrix and then reverse each row. Transpose the upper triangle(excluding diagonal) of the matrix by swapping the elements at (i, j) and (j, i). Then reverse each row of the matrix by swapping the elements at (i, j) and (i, n-j-1).
+    // Time Complexity: O(n^2) where n is the number of rows (or columns) in the given matrix. We are traversing the matrix twice, once to transpose it and once to reverse each row.
+    // Space Complexity: O(1) as we are not using any extra space.
+    public void rotateOptimalVersion2(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the upper triangle(excluding diagonal) by swapping
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row by swapping
+        for(int i=0; i<n; i++){
+            int left = 0, right = n-1;
+
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
