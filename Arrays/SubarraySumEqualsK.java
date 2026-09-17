2ad2cb90a9f7292c26a6a3127eb37277ca1d41ad
raaviarora01 <aroraraavi01@gmail.com>
2026-09-17 23:39:06 +0530

    feat: add solution implementation for SubArraySumEqualsK using optimal approach

--- Arrays/SubarraySumEqualsK.java
+++ Arrays/SubarraySumEqualsK.java
@@ -4,6 +4,9 @@ A subarray is a contiguous non-empty sequence of elements within an array. */
 
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumEqualsK {
     
     // Brute Force Approach: Iterate through the array and check all possible sub-arrays to find the ones that sum to k.
@@ -20,6 +23,28 @@ public class SubarraySumEqualsK {
                     count++;
                 }
             }
+        }
+
+        return count;
+    }
+
+    // Optimal Approach: Use HashMap to store the sum of elements from the start of the array to the current index. If the difference between the current sum and k exists in the HashMap, it means there is a sub-array that sums to k.
+    // Time Complexity: O(n) - The method iterates over input array once.
+    // Space Complexity: O(n) - The method uses a HashMap to store the sum of elements from the start of the array to the current index, which can take up to n space in the worst case.
+    public int subarraySumOptimalApproach(int[] nums, int k) {
+        int count = 0;
+        long sum = 0;
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1);
+
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+
+            if(map.containsKey(sum - k)){
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         return count;
