f0cdc4f4fd6411654b0f657092206e516e6a5d62
raaviarora01 <aroraraavi01@gmail.com>
2026-09-23 00:48:01 +0530

    feat: add solution for ProductOfArrayExceptItself using Optimal approach

--- Arrays/ProductOfArrayExceptSelf.java
+++ Arrays/ProductOfArrayExceptSelf.java
@@ -10,7 +10,7 @@ public class ProductOfArrayExceptSelf {
     
     // Brute Force Approach: Iterate through the array and for each element, calculate the product of all other elements.
     // Time Complexity: O(n^2) - The method iterates over input array twice
-    // Space Complexity: O(n) - The method uses an additional array to store the result
+    // Space Complexity: O(n) - The method uses an additional array to store the result, o(1) because output array is required by question
     public int[] productExceptSelf(int[] nums) {
         int[] products = new int[nums.length];
         for(int i=0; i<nums.length; i++){
@@ -49,6 +49,27 @@ public class ProductOfArrayExceptSelf {
 
         for(int i=0; i<n; i++){
             ans[i] = prefix[i] * suffix[i];
+        }
+
+        return ans;
+    }
+
+    // Optimal Approach: Use output array to store the prefix products and a variable to keep track of the suffix product. The prefix product at index i is the product of all elements before index i, and the suffix product is updated as we iterate from the end of the array to the beginning. The final result for each index is the product of the prefix and suffix products.
+    // Time Complexity: O(n) - The method iterates over input array twice.
+    // Space Complexity: O(1) - The method uses a constant amount of space for variables to store the prefix and suffix products, o(1) because output array is required by question
+    public int[] productExceptSelfOptimalApproach(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        ans[0] = 1;
+        for(int i=1; i<n; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        int suffix=1;
+        for(int i=n-1; i>=0; i--){
+            ans[i] = ans[i] * suffix;
+            suffix = suffix * nums[i];
         }
 
         return ans;
